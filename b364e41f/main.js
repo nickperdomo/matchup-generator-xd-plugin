75512c9cc352bc6ca34b63d2df62d48c6beecf34
Nick Perdomo <nickperdomo121@gmail.com>
2018-12-13 11:44:23 -0800

    Initial testing of network request works. Sheetsu API is providing JSON that includes image paths for logos.

---
+++ b364e41f/main.js
@@ -0,0 +1,68 @@
+/*
+ * Sample plugin scaffolding for Adobe XD.
+ *
+ * 
+ */
+
+const { Rectangle, Color, ImageFill } = require('scenegraph');
+const { xhrBinary, base64ArrayBuffer } = require('./utils/network');
+// const xhrBinary = require('./network').xhrBinary;
+
+
+// function myPluginCommand(selection) {
+//     // Go to Plugins > Development > Developer Console to see this log output
+//     console.log("Plugin command is running!");
+
+//     // Insert a red square at (0, 0) in the current artboard or group/container
+//     var shape = new Rectangle();
+//     shape.width = 100;
+//     shape.height = 100;
+//     shape.fill = new Color("lime");
+//     // selection.insertionParent.addChild(shape);
+// }
+
+
+
+
+function myPluginCommand(selection) {
+
+    if (selection.items.length) {
+        const url = "https://sheetsu.com/apis/v1.0su/8c894eb7a43d";
+        return fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (jsonResponse) {
+                return downloadImage(selection, jsonResponse);
+            });
+    } else {
+        console.log("Please select a shape to apply the downloaded image.");
+    }
+
+}
+
+async function downloadImage(selection, jsonResponse) {
+    try {
+
+        const photoUrl = jsonResponse[2].logo;
+        const photoObj = await xhrBinary(photoUrl);
+        const photoObjBase64 = await base64ArrayBuffer(photoObj);
+        applyImagefill(selection, photoObjBase64);
+
+    } catch (err) {
+        console.log("error")
+        console.log(err.message);
+    }
+}
+
+function applyImagefill(selection, base64) {
+    const imageFill = new ImageFill(`data:image/png;base64,${base64}`);
+    selection.items[0].fill = imageFill;
+}
+
+
+module.exports = {
+    commands: {
+        myPluginCommand: myPluginCommand
+    }
+};
