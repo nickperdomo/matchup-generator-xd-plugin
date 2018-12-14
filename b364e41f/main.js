f9f7f77ad7fedba7c719db1d5c4b61f82a427493
Nick Perdomo <Nick.Perdomo@Foxsports.net>
2018-12-13 16:47:04 -0800

    Added Setup Dialog promise that prevents script from running

--- b364e41f/main.js
+++ b364e41f/main.js
@@ -1,13 +1,10 @@
-/*
- * Sample plugin scaffolding for Adobe XD.
- *
- * 
+/* Created by Nick Perdomo
+ * Questions, praise, hate? Email: nickperdomo121@gmail.com
  */
 
-const { Rectangle, Color, ImageFill } = require('scenegraph');
-const { xhrBinary, base64ArrayBuffer } = require('./utils/network');
-// const xhrBinary = require('./network').xhrBinary;
-
+const { Rectangle, Color, ImageFill } = require("scenegraph");
+const { xhrBinary, base64ArrayBuffer } = require("./utils/network");
+const { setupDialog } = require("./ui/modal");
 
 // function myPluginCommand(selection) {
 //     // Go to Plugins > Development > Developer Console to see this log output
@@ -22,61 +19,55 @@ const { xhrBinary, base64ArrayBuffer } = require('./utils/network');
 // }
 
 // TODO: Capture user input from plugin UI to set sportCode
-const sportCode = 'NFL'
-
-
-
+const sportCode = "NFL";
 
 // Main function fires when user click on it in the Plugins menu.
 function myPluginCommand(selection) {
-    
-    const idJSON = ((sport) => {
-        switch(sport) {
-            case 'NFL':
-                return 'https://sheetsu.com/apis/v1.0su/8c894eb7a43d';
-            default:
-                return null; 
+  setupDialog.showModal().then(result => {
+      const idJSON = (sport => {
+        switch (sport) {
+          case "NFL":
+            return "https://sheetsu.com/apis/v1.0su/8c894eb7a43d";
+          default:
+            return null;
         }
-    })(sportCode);
+      })(sportCode);
 
-    if (selection.items.length) {
+      if (selection.items.length) {
         return fetch(idJSON)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (jsonResponse) {
-                return downloadImage(selection, jsonResponse);
-            });
-    } else {
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(jsonResponse) {
+            return downloadImage(selection, jsonResponse);
+          });
+      } else {
         console.log("Please select a shape to apply the downloaded image.");
+      }
+
+      async function downloadImage(selection, jsonResponse) {
+        try {
+          const photoUrl = jsonResponse[4].logo;
+          const photoObj = await xhrBinary(photoUrl);
+          const photoObjBase64 = await base64ArrayBuffer(photoObj);
+          applyImagefill(selection, photoObjBase64);
+
+          console.log(photoUrl);
+        } catch (err) {
+          console.log("error");
+          console.log(err.message);
+        }
+      }
+    });
+
+    function applyImagefill(selection, base64) {
+      const imageFill = new ImageFill(`data:image/png;base64,${base64}`);
+      selection.items[0].fill = imageFill;
     }
-
 }
-
-async function downloadImage(selection, jsonResponse) {
-    try {
-
-        const photoUrl = jsonResponse[14].logo;
-        const photoObj = await xhrBinary(photoUrl);
-        const photoObjBase64 = await base64ArrayBuffer(photoObj);
-        applyImagefill(selection, photoObjBase64);
-
-        console.log(photoUrl)
-
-    } catch (err) {
-        console.log("error")
-        console.log(err.message);
-    }
-}
-
-function applyImagefill(selection, base64) {
-    const imageFill = new ImageFill(`data:image/png;base64,${base64}`);
-    selection.items[0].fill = imageFill;
-}
-
 
 module.exports = {
-    commands: {
-        myPluginCommand: myPluginCommand
-    }
+  commands: {
+    myPluginCommand: myPluginCommand
+  }
 };
