87869b66eb88e7dc81e01e9e7fe54b84cd2af81d
Nick Perdomo <Nick.Perdomo@Foxsports.net>
2019-01-04 09:24:57 -0800

    Fetch is working now

--- b364e41f/main.js
+++ b364e41f/main.js
@@ -24,6 +24,7 @@ const sportCode = "NFL";
 
 // The main function fires when a user clicks the menu item in Plugins.
 function myPluginCommand(selection, documentRoot) {
+    // return statement of plugin handler
     return setupDialog.showModal()
     .then((result) => {
         let docNode = documentRoot;
@@ -52,98 +53,99 @@ function myPluginCommand(selection, documentRoot) {
 
         const sheetsuEndpoint = 'https://sheetsu.com/apis/v1.0su/8c894eb7a43d/sheets/exportList';
 
-        // return statement of plugin handler
         fetch(sheetsuEndpoint)
-            .then( (response) => {
-                const jsonResponse = response.json();
+            .then( response => response.json() )
+            .then( jsonResponse => {
                 console.log(jsonResponse);
-                let dataset = {
-                    jsonResponse: jsonResponse,
-                    exportableAssets: exportableAssets,
-                    homeLogoConts: homeLogoConts,
-                    awayLogoConts: awayLogoConts,
-                }        
-                return dataset;
-            })
-            .catch( (reason) => {
-                return console.log(`Reason: ${reason}`)
-            })
+            });
+                
+        //         let dataset = {
+        //             jsonResponse: jsonResponse,
+        //             exportableAssets: exportableAssets,
+        //             homeLogoConts: homeLogoConts,
+        //             awayLogoConts: awayLogoConts,
+        //         }        
+        //         return dataset;
+        //     })
+        //     .catch( (reason) => {
+        //         return console.log(`Reason: ${reason}`)
+        //     })
         })    
-    .then( (resolvedValue) => {
-        return (
-            // console.log(resolvedValue.jsonResponse),
-            downloadImage(resolvedValue.homeLogoConts, resolvedValue.jsonResponse, "home"),
-            downloadImage(resolvedValue.awayLogoConts, resolvedValue.jsonResponse, "away")
-        );
-    })
-    .then( function () {
-        return exportRenditions(exportableAssets); 
-    });
+    // .then( (resolvedValue) => {
+    //     return (
+    //         console.log(resolvedValue)
+    //         // downloadImage(resolvedValue.homeLogoConts, resolvedValue.jsonResponse, "home"),
+    //         // downloadImage(resolvedValue.awayLogoConts, resolvedValue.jsonResponse, "away")
+    //     );
+    // })
+    // .then( function () {
+    //     return exportRenditions(exportableAssets); 
+    // });
     
 
 
-        async function exportRenditions(exportableAssets) {
-            try {
-                if (exportableAssets.length > 0) {
-                    const folder = await fs.getFolder();
-                    const arr = await exportableAssets.map(async asset => {		
-                        const file = await folder.createFile(`${asset.name}.png`, {overwrite: true});
-                        let obj = {
-                            node: asset,               
-                            outputFile: file,                    
-                            type: application.RenditionType.PNG,    
-                            scale: 2   
-                        }
-                        return obj
-                    });
-                    const renditions = await Promise.all(arr);
+    //     async function exportRenditions(exportableAssets) {
+    //         try {
+    //             if (exportableAssets.length > 0) {
+    //                 const folder = await fs.getFolder();
+    //                 const arr = await exportableAssets.map(async asset => {		
+    //                     const file = await folder.createFile(`${asset.name}.png`, {overwrite: true});
+    //                     let obj = {
+    //                         node: asset,               
+    //                         outputFile: file,                    
+    //                         type: application.RenditionType.PNG,    
+    //                         scale: 2   
+    //                     }
+    //                     return obj
+    //                 });
+    //                 const renditions = await Promise.all(arr);
      
-                    await application.createRenditions(renditions)    
-                        .then(results => {                             
-                            console.log(`Renditions have been saved at ${results[0].outputFile.nativePath}`);
-                        })
-                        .catch(error => {                             
-                            console.log(error);
-                        });
+    //                 await application.createRenditions(renditions)    
+    //                     .then(results => {                             
+    //                         console.log(`Renditions have been saved at ${results[0].outputFile.nativePath}`);
+    //                     })
+    //                     .catch(error => {                             
+    //                         console.log(error);
+    //                     });
 
-                }
-            } catch (err) {
-                console.log("error");
-                console.log(err.message);
-            }
-        } 
+    //             }
+    //         } catch (err) {
+    //             console.log("error");
+    //             console.log(err.message);
+    //         }
+    //     } 
 
-        async function downloadImage(logoConts, jsonResponse, team) {
-            try {
-                const logoSide = (team => {
-                    switch (team) {
-                        case "home":
-                            return "homeTeamLogoURL";
-                        case "away":
-                            return "awayTeamLogoURL";  
-                        default:
-                            return null;
-                    }
-                })(team);
-                const logoUrl = jsonResponse[0][logoSide];
-                const logoObj = await xhrBinary(logoUrl);
-                const logoObjBase64 = await base64ArrayBuffer(logoObj);
-                applyImagefill(logoConts, logoObjBase64);
+    //     async function downloadImage(logoConts, jsonResponse, team) {
+    //         try {
+    //             const logoSide = (team => {
+    //                 switch (team) {
+    //                     case "home":
+    //                         return "homeTeamLogoURL";
+    //                     case "away":
+    //                         return "awayTeamLogoURL";  
+    //                     default:
+    //                         return null;
+    //                 }
+    //             })(team);
+    //             const logoUrl = jsonResponse[0][logoSide];
+    //             const logoObj = await xhrBinary(logoUrl);
+    //             const logoObjBase64 = await base64ArrayBuffer(logoObj);
+    //             applyImagefill(logoConts, logoObjBase64);
 
-                console.log(logoUrl);
+    //             console.log(logoUrl);
 
-            } catch (err) {
-                console.log("error");
-                console.log(err.message);
-            }
-        }
+    //         } catch (err) {
+    //             console.log("error");
+    //             console.log(err.message);
+    //         }
+    //     }
     
-        function applyImagefill(logoConts, base64) {
-            const imageFill = new ImageFill(`data:image/png;base64,${base64}`);
-            logoConts.forEach(container =>
-                container.fill = imageFill
-            );
-        }
+    //     function applyImagefill(logoConts, base64) {
+    //         const imageFill = new ImageFill(`data:image/png;base64,${base64}`);
+    //         logoConts.forEach(container =>
+    //             container.fill = imageFill
+    //         );
+    //     }
 
 } //modal closing bracket
 
