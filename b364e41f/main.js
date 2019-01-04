a7a8d485e552077cdcd6880336b54d66b4cc4d6a
Nick Perdomo <Nick.Perdomo@Foxsports.net>
2019-01-03 16:13:48 -0800

    Initial refactor of promises

--- b364e41f/main.js
+++ b364e41f/main.js
@@ -1,5 +1,5 @@
 /* Created by Nick Perdomo
- * Questions, praise, hate? Email: nickperdomo121@gmail.com
+ * Questions, praise, hate? Sling it here: nickperdomo121@gmail.com
  */
 const application = require("application");
 const fs = require("uxp").storage.localFileSystem;
@@ -24,10 +24,9 @@ const sportCode = "NFL";
 
 // The main function fires when a user clicks the menu item in Plugins.
 function myPluginCommand(selection, documentRoot) {
-    return setupDialog.showModal().then(result => {
+    return setupDialog.showModal()
+    .then((result) => {
         let docNode = documentRoot;
-        // let node = selection.items[0];
-        // console.log("The selected node is a: " + node.constructor.name);
         
         // Capture exportableAssets(exportable assets) and team logo containers
         const exportableAssets = docNode.children.filter(child => child.markedForExport);
@@ -43,8 +42,6 @@ function myPluginCommand(selection, documentRoot) {
                     child.name === 'awayLogoContainer'
                 )[0]
             );
-   
-        // console.log(awayLogoConts);
 
         // node.children.forEach(function (childNode, i) {
         //     console.log("Child " + i + " type: " + childNode.constructor.name);
@@ -53,26 +50,36 @@ function myPluginCommand(selection, documentRoot) {
 
         // });
 
-        // TODO: Remove sport selector and pull all required data from JSON
-        // which should include logo urls and tricodes for file names
-        const idJSON = 'https://sheetsu.com/apis/v1.0su/8c894eb7a43d/sheets/exportList';
+        const sheetsuEndpoint = 'https://sheetsu.com/apis/v1.0su/8c894eb7a43d/sheets/exportList';
 
         // return statement of plugin handler
-        return fetch(idJSON)
-            .then(function (response) {
-                return response.json();
+        fetch(sheetsuEndpoint)
+            .then( (response) => {
+                const jsonResponse = response.json();
+                console.log(jsonResponse);
+                let dataset = {
+                    jsonResponse: jsonResponse,
+                    exportableAssets: exportableAssets,
+                    homeLogoConts: homeLogoConts,
+                    awayLogoConts: awayLogoConts,
+                }        
+                return dataset;
             })
-            .then(function (jsonResponse) {
-                return (
-                    console.log(jsonResponse),
-                    downloadImage(homeLogoConts, jsonResponse, "home"),
-                    downloadImage(awayLogoConts, jsonResponse, "away")
-                );
+            .catch( (reason) => {
+                return console.log(`Reason: ${reason}`)
             })
-            .then( function () {
-                return exportRenditions(exportableAssets); 
-            });
-        
+        })    
+    .then( (resolvedValue) => {
+        return (
+            // console.log(resolvedValue.jsonResponse),
+            downloadImage(resolvedValue.homeLogoConts, resolvedValue.jsonResponse, "home"),
+            downloadImage(resolvedValue.awayLogoConts, resolvedValue.jsonResponse, "away")
+        );
+    })
+    .then( function () {
+        return exportRenditions(exportableAssets); 
+    });
+    
 
 
         async function exportRenditions(exportableAssets) {
@@ -137,7 +144,7 @@ function myPluginCommand(selection, documentRoot) {
                 container.fill = imageFill
             );
         }
-    });
+
 } //modal closing bracket
 
 module.exports = {
